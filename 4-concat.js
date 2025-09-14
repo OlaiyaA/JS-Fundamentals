995387c8a72cd4c7b73502c581ec5affc86b6d22
Abdulmalik Olaiya <olaiya.abdulmalik@gmail.com>
2025-09-14 17:28:31 +0100

    added 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+// 4-concat.js
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(arg1 + " is " + arg2);
+// Example: node 4-concat.js JavaScript amazing
+// Output: JavaScript is amazing    
