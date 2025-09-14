94661734db951d200fbda123e0462d368e6f9897
Abdulmalik Olaiya <olaiya.abdulmalik@gmail.com>
2025-09-14 22:12:49 +0100

    added 10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+// 10-factorial.js
+
+function factorial(n) {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = Number(process.argv[2]);
+console.log(factorial(num));
